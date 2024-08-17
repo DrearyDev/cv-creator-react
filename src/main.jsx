c3fa7e6a9b9c027237d55a99281a08c3739f2c69
Dreary Dev <143649090+DrearyDev@users.noreply.github.com>
2024-08-16 21:53:59 -0400

    Add default text for all sections
    text that will be shown as the defaults for
    the template cv and can be changed in edit
    mode.

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,91 @@ import { useState } from 'react'
 
 import './styles/index.css'
 
-import Form from './components/form.jsx'
-import Cv from './components/cv.jsx'
+
+const headerInputs = {
+    //["Content to display", "Optional Link"]
+    name: ["Jhon Doe", ""],
+    phoneNum: ["123-456-7890", ""],
+    email: ["fakeemail@email.com", "mailto:fakeemail@email.com"],
+    github: ["jhondoe", "https://github.com/jhondoe"],
+    linkedin: ["jhondoe4", ""]
+};
+
+const proSummary = `Hardworking Software Engineer with more than five years of experience
+with frontend development and 3 years of experience with full-stack development.`;
+
+const education = [
+    {
+        school: "Carter G Woodson High-School",
+        gradYear: "2016 - 2020",
+        studyField: "", // leaving blank should not render anything for that option
+        degree: "High school Degree"
+    },
+    {
+        school: "Fake School University",
+        gradYear: "2020 - 2024",
+        studyField: "Computer Science",
+        degree: "Computer Science"
+    },
+];
+
+const workExp = [
+    {
+        jobTitle: "Frontend Software Engineer",
+        employmentDates: "2002 - 2020",
+        bulletPoints: [
+            "Utilize CSS to style webpages (frontend)",
+            "Utilize React to add functionality to webpages (frontend)",
+            "Utilize JSX with React to streamline development"
+        ]
+    },
+    {
+        jobTitle: "Fullstack Software Engineer",
+        employmentDates: "2021 - 2024",
+        bulletPoints: [
+            "Utilize Node to validate user input (backend)",
+            "Validate/Store Passwords for user Accounts",
+            "Further skills with Frontend tools"
+        ]
+    }
+];
+
+const skills = [
+    "HTML",
+    "CSS",
+    "Javascript",
+    "React",
+    "Linux",
+    "Node",
+    "JSON",
+    "JSX"
+];
+
+const certs = [
+    //["Name of certification", "Optional link to Certification"]
+    ["Comptia A+", "https://www.comptia.org/certifications/a"],
+    ["Comptia Network+", "https://www.comptia.org/certifications/network"],
+    ["Comptia Security+", "https://www.comptia.org/certifications/security"],
+    ["Comptia Linux+", "https://www.comptia.org/certifications/linux"]
+];
+
+function HandleVisible({headerInputs, proSummary, education, workExp, skills, certs}) {
+    return(
+        <h1>Hello from main.jsx</h1>
+    );
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Form/>
-        <Cv/>
-    </React.StrictMode>,
+        <HandleVisible
+            headerInputs = {headerInputs}
+            proSummary = {proSummary}
+            education = {education}
+            workExp = {workExp}
+            skills = {skills}
+            certs = {certs}
+        />
+    </React.StrictMode>
 )
 
 /*
